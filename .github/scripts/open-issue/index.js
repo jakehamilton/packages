16efd6e02c33b7f64acef9f46f13c8117e148f75
Jake Hamilton <jake.hamilton@hey.com>
2020-09-09 23:55:41 -0700

    chore(ci): add issue workflow

---
+++ .github/scripts/open-issue/index.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const path = require("path");
+const littlelog = require("@littlethings/log");
+
+const log = littlelog.create("Auto Issue");
+
+littlelog.setVerbosity("INFO");
+
+module.exports = async ({ github, context }) => {
+    const creator = context.payload.sender.login;
+
+    const opts = github.issues.listForRepo.endpoint.merge({
+        ...context.issue,
+        creator,
+        state: "all",
+    });
+
+    const issues = await github.paginate(opts);
+
+    let isFirstSubmission = true;
+    for (const issue of issues) {
+        if (issue.number === context.issue.number) {
+            continue;
+        }
+
+        if (issue.pull_request) {
+            isFirstSubmission = false;
+            break;
+        }
+    }
+
+    if (isFirstSubmission) {
+        log.info(`First submission from "${creator}".`);
+        const body = fs.readFileSync(path.resolve(__dirname, "welcome.md"));
+
+        await github.issues.createComment({
+            issue_number: context.issue.number,
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            body,
+        });
+    }
+
+    // @TODO(jakehamilton): check for package name
+    // const issueBody = context.payload.issue.body;
+};
---
+++ .github/scripts/open-issue/welcome.md
@@ -0,0 +1,5 @@
+👋 Hi there!
+
+It looks like this is your first issue on this project.
+Thanks for filing it! If you haven't already, please
+take a look at the [Code of Conduct](https://github.com/jakehamilton/packages/blob/master/CODE-OF-CONDUCT.md).
